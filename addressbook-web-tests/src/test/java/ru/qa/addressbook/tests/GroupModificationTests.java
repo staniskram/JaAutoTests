20520160810c69b63ada98ac2b4b5f4365d24549
staniskram <stanislav-kuritsyn@rambler.ru>
2021-04-20 08:06:59 +0300

    add test group modification

---
+++ addressbook-web-tests/src/test/java/ru/qa/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.qa.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.qa.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gottoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
