85efbdf13c2ed856182b2821aa8fd05a9f1356eb
staniskram <stanislav-kuritsyn@rambler.ru>
2021-06-25 13:49:37 +0300

    added new module for mantis

---
+++ mantis-tests/src/test/java/ru/st/mantisbt/tests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.st.mantisbt.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.st.mantisbt.appmanager.ApplicationManager;
+import java.io.IOException;
+
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite
+  public void setUp() throws IOException {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+}
